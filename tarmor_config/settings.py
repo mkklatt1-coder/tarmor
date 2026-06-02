0b65d0698a5992ae2b7fe34cdfa33573130c1920
Mark Klatt <mkklatt1@gmail.com>
2026-06-02 07:12:11 -0500

    Fix stylesheet

--- tarmor_config/settings.py
+++ tarmor_config/settings.py
@@ -172,7 +172,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
