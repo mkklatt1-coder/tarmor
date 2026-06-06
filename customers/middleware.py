5fa61ca72ac74da3fb59e7a3a79ceb03d586aba5
Mark Klatt <mkklatt1@gmail.com>
2026-06-05 21:45:31 -0500

    Update middleware to address logout

--- customers/middleware.py
+++ customers/middleware.py
@@ -2,7 +2,6 @@ from django.db import connection
 from django.shortcuts import redirect
 from django.urls import reverse
 from django_tenants.utils import get_tenant_model
-
 class SessionTenantMiddleware:
     PUBLIC_PATH_PREFIXES = (
         "/static/",
@@ -17,8 +16,6 @@ class SessionTenantMiddleware:
         login_url = reverse("login")
         logout_url = reverse("logout")
         path = request.path_info
-        print("MIDDLEWARE PATH:", path)
-        print("SESSION tenant_schema:", request.session.get("tenant_schema"))
         try:
             if (
                 path == login_url
@@ -26,13 +23,12 @@ class SessionTenantMiddleware:
                 or path in self.PUBLIC_EXACT_PATHS
                 or any(path.startswith(prefix) for prefix in self.PUBLIC_PATH_PREFIXES)
             ):
-                print("PUBLIC PATH")
                 connection.set_schema_to_public()
                 request.tenant = None
                 return self.get_response(request)
             schema_name = request.session.get("tenant_schema")
             if not schema_name or schema_name == "public":
-                print("NO VALID TENANT SCHEMA - REDIRECT LOGIN")
+                request.session.pop("tenant_schema", None)
                 connection.set_schema_to_public()
                 request.tenant = None
                 return redirect("login")
@@ -40,12 +36,10 @@ class SessionTenantMiddleware:
             try:
                 tenant = Tenant.objects.get(schema_name=schema_name)
             except Tenant.DoesNotExist:
-                print("TENANT DOES NOT EXIST - REDIRECT LOGIN")
                 request.session.pop("tenant_schema", None)
                 connection.set_schema_to_public()
                 request.tenant = None
                 return redirect("login")
-            print("SETTING TENANT:", tenant.schema_name)
             request.tenant = tenant
             connection.set_tenant(tenant)
             return self.get_response(request)
